69ce3c025eedcedde5a17dbd24bc743802258cca
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-23 10:35:14 -0700

    add .all method for definition; test good fail

---
+++ lib/definition.rb
@@ -0,0 +1,16 @@
+class Definition
+  attr_reader :id
+  attr_accessor :name
+
+  @@words = {}
+  @@total_rows = 0
+
+  def initialize (attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id) || @@total_rows +=1
+  end
+
+  # def self.all 
+  #   @@words.values
+  # end
+end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -2,3 +2,16 @@ require 'simplecov'
 SimpleCov.start
 require 'rspec'
 require 'definition'
+
+describe '#definition' do 
+
+  # before(:each) do
+  #   Definition.clear()
+  # end
+
+  describe('.all') do
+    it("it will return an empty array of definitions.") do
+      expect(Definition.all).to(eq([]))
+    end
+  end
+end
